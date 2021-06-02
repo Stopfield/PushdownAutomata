9598db0b0e8446a0608ce28880914a8b52a2d139
Nobre Lucas <lucasbnobre18@gmail.com>
2021-06-01 23:51:37 -0300

    Added the PushDownAutomata constructor

--- automata.py
+++ automata.py
@@ -1,24 +1,14 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+class PushDownAutomata:
 
-automata = nx.DiGraph()
+    def __init__ (self, states, alphabet, transition_function):
+        # Defines the symbols the automata can recognize
+        self.sigma = alphabet
 
-automata.add_node('q0', state='start state')
-automata.add_node('q1')
-automata.add_node('qf', state='final state')
+        # Defines the states, and their atributes
+        self.states = states
+        self.start = self.define_start_state()
+        self.final = self.define_final_state()
+        self.current_state = self.start
 
-# edge_list = [('q0', 'q0'), ('q0', 'q1'), ('q1', 'q1'), ('q0', 'qf'), ('q1', 'qf')]
-# automata.add_edges_from(edge_list)
-
-automata.add_edge('q0', 'q0', accept = '(a, void, B)')
-automata.add_edge('q0', 'q1', accept = '(b, B, void)')
-automata.add_edge('q0', 'qf', accept = '(?, ?, void)')
-automata.add_edge('q1', 'q1', accept = '(b, B, void)')
-automata.add_edge('q1', 'qf', accept = '(?, ?, void)')
-
-print(automata.nodes)
-print(automata.edges)
-
-plt.subplot(111)
-nx.draw(automata)
-plt.show()
+        # Defines the transition funcion that defines the automata
+        self.delta = transition_function
