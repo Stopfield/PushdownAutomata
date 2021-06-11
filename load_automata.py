e4212209a02b4c478ee3e53f8816f2e177967510
Moreno <monorease@gmail.com>
2021-06-11 18:54:04 -0300

    Updated load_automata.py

--- load_automata.py
+++ load_automata.py
@@ -8,6 +8,10 @@ def components():
     # Regex para remoção de símbolos inúteis.
     regex = re.compile(r'[^a-zA-Z0-9]+')
 
+    # Tratamento de erro no arquivo.
+    if file[0].count(',') != file[0].count(' '):
+        raise Exception("Erro na separacao dos componentes!")
+
     # Loop que separa os conjuntos em tuplas.
     component = []
     for i in str(file[:1]).split(','):
@@ -23,15 +27,21 @@ def components():
         else:
             component.append(regex.sub('', i))
 
-    # Tratamento de erros do arquivo.
-    if file.count(',') != file.count(' '):
-        raise Exception("Erro na separacao dos componentes!")
-    elif len(component) != 6:
-        raise Exception("Erro nos componentes do automato!")
-    elif 'D' not in component:
-        raise Exception("Erro na letra do conjunto de regras de producao!")
+    # Tratamento de padrão dos componentes.
+    for i in component[1]:
+        if not re.match(r'q[0-9]', i):
+            raise Exception('Erro no padrao dos estados!')
+    for i in component[0]:
+        if not re.match(r'[a-z]', i):
+            raise Exception('Erro no padrao dos simbolos!')
+    for i in component[5]:
+        if not re.match(r'[A-Z]', i):
+            raise Exception('Erro no padrao dos simbolos!')
+    if 'D' != component[2]:
+        raise Exception('Erro na letra do conjunto de regras de producao!')
     else:
         return tuple(component)
+
 
 def functions():
     # Loop que separa as funções de transição em tuplas.
@@ -39,6 +49,9 @@ def functions():
     for line in file[1:]:
         function.append(tuple([c.strip() for c in line.split(',')]))
 
+    # Tratamento de padrão das funções de transição.
+    for line in function:
+        for word in line:
+            if not re.match(r'[a-zA-Z0-9?-]', word):
+                raise Exception("Erro: Funcao de Transicao.")
     return tuple(function)
-
-# print(components())
