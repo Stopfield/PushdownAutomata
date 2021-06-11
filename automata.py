2c5cdd34e9a9de7a6bc3a331b7279045d8fd6920
Moreno <monorease@gmail.com>
2021-06-11 15:32:31 -0300

    Updated automata.py

--- automata.py
+++ automata.py
@@ -1,7 +1,3 @@
-from load_automata import *
-
-# print(components()[0])
-
 class Stack:
 
     def __init__ (self):
@@ -18,21 +14,39 @@ class Stack:
 
 class PushDownAutomata:
 
-    def __init__ (self, states = components()[1], alphabet = components()[0]):
+    def __init__ (self, states, alphabet, transition_function, stack_alphabet):
         # Defines the symbols the automata can recognize
-        self.sigma = alphabet
+        self.sigma = alphabet # {a, b}
 
         # Defines the states, and their atributes
-        self.states = states
-        # self.start = self.define_start_state()
-        # self.final = self.define_final_state()
-        # self.current_state = self.start
+        self.states = states # {q0, ..., qf}
+        self.start = self.define_start_state()
+        self.final = self.define_final_state()
+        self.current_state = self.start
 
-        # # Defines the transition funcion that defines the automata
-        # self.delta = transition_function
+        # Defines the transition funcion that defines the automata
+        self.delta = self.populate_transition_function()
 
-        # # Defines the stack atributes
-        # self.stack = Stack()
-        # self.V = stack_alphabet
+        # Defines the stack atributes
+        self.stack = Stack()
+        self.V = stack_alphabet
 
-print(PushDownAutomata().sigma)
+    def populate_transition_function(self):
+        
+        """
+        Populate the delta atribute as a dictionary of dictionarys,
+        simulation the behavior of a automata ((state, word) = (state'))
+        """
+
+        transition_dict = {state : {element : 'Reject' for element in self.sigma} for state in self.states}
+
+        for state, dict_value in transiction_dict.items():
+            print(f'Enter transitions for state {state}. If required, use "Reject"')
+
+            for input_alphabet, transition_state in dict_value.items():
+                transition_dict[key][input_alphabet] = input(f'Current State: {key}\tInput Alphabet: {input_alphabet}\tNext State : ')
+
+        print('\n Transiction Function Q x Sigma -> Q')
+        print('Current State\tInput Alphabet\tNext State')
+        for key, dict_value in transiction_dict.items():
+            print(f'{key}\t\t{input_alphabet}\t\t{transition_state}')
